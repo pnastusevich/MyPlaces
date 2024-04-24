446a6d5500568605851f88059afd0a9fa100b513
pnastusevich <pasha.nastusevich@gmail.com>
2024-04-24 23:14:59 +0300

    added the ability to set ratings for establishments

---
+++ MyPlaces/RatingControl.swift
@@ -0,0 +1,20 @@
+//
+//  RatingControl.swift
+//  MyPlaces
+//
+//  Created by Паша Настусевич on 23.04.24.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
